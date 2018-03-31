475d0dc1a402e979d6a2d837cce660cc6e925d2b
hqweay <1120073230@qq.com>
2018-03-31 21:25:13 +0800

    fix some problems

--- src/main/java/com/swpu/jobanalysissystem/controller/JobAnalysisController.java
+++ src/main/java/com/swpu/jobanalysissystem/controller/JobAnalysisController.java
@@ -4,6 +4,7 @@ import com.swpu.jobanalysissystem.dao.JobInfoMapper;
 import com.swpu.jobanalysissystem.dao.RecommendJobImageMapper;
 import com.swpu.jobanalysissystem.entity.JobInfo;
 import com.swpu.jobanalysissystem.entity.JobAnalysis;
+import com.swpu.jobanalysissystem.entity.RecommedJobImage;
 import com.swpu.jobanalysissystem.until.ContentRecommedStrategy;
 import com.swpu.jobanalysissystem.until.SqlQueryRecommedStrategy;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,6 +13,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 
@@ -44,20 +47,19 @@ public class JobAnalysisController {
     @RequestMapping(value = "/job-analysis.html", method = POST)
     public String getUserInformation(@ModelAttribute("jobAnalysis") JobAnalysis jobAnalysis, Model model){
 //        ArrayList<Integer> jobIds = new ArrayList<Integer>();
-        //得到list
+//        //得到list
 //        List<RecommedJobImage> recommedJobImages = recommendJobImageMapper.getRecommenedJobImages();
 //        //web前端传来的user信息封装
 //        HashMap<String,Double> user = new HashMap<>();
 //        user.put(jobAnalysis.getXueli(),1.0);
-//        user.put(jobAnalysis.getZhuanye(),1.0);
-//        user.put(jobAnalysis.getXuexiao(),1.0);
+//
 //        user.put(jobAnalysis.getQiuzhidi(),5.0);
 //        user.put(jobAnalysis.getGongzuonianxian(),1.0);
 //        user.put(jobAnalysis.getJineng(),1.0);
-//        user.put(jobAnalysis.getJingli(),1.0);
+//
 //        //获得job的id集合并格式化
 //        jobIds = contentRecommedStrategy.getRecommendJobIds(user);
-        //格式化一下
+//        //格式化一下
 //        String ids = jobIds.toString().replace("[", "").replace("]","");
 //        //通过格式化的id集合找到job集合
 //        List<JobInfo> jobs = jobInfoMapper.getRecommenedJobs(ids);
